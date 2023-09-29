5a5a92db32562933ca2c9caf7317aef1665868f5
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2023-09-29 16:34:25 +0500

    change type of User Id of Telegramm

--- backend-quiz-bot/ViewModels/UserAlsoCheckAnswerVM.cs
+++ backend-quiz-bot/ViewModels/UserAlsoCheckAnswerVM.cs
@@ -2,7 +2,7 @@ namespace BackendQuizBot.ViewModels;
 
 public class UserAlsoCheckAnswerVM
 {
-    public string UserId { get; set; }
+    public long UserId { get; set; }
     public List<CheckAnswerVM> AnswerAndQuestions { get; set; }
     public int SpentTime { get; set; }
 }
